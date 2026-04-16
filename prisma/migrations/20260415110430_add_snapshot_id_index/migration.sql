8633298259590b71179493bc2ebf26fa576fd4da
AritaYuto <marisa.love0925@gmail.com>
2026-04-16 10:38:27 +0900

    add 20260415110430_add_snapshot_id_index

---
+++ prisma/migrations/20260415110430_add_snapshot_id_index/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "asset_entries_snapshot_id_idx" ON "asset_entries"("snapshot_id");
+
+-- CreateIndex
+CREATE INDEX "asset_links_snapshot_id_idx" ON "asset_links"("snapshot_id");
